fc27fb4a94c61dd728db1c9aa09c3d2830416af9
Pierce McGoran <pmcgoran@students.kennesaw.edu>
2020-03-06 13:44:04 -0500

    Added driver logic, fixed algorithm slightly, added exceptions

--- Assignment4/BaseConversion.cs
+++ Assignment4/BaseConversion.cs
@@ -12,7 +12,31 @@ using System;
 namespace Assignment4 {
     public class BaseConversion {
         public static void Main(string[] args) {
-            Console.WriteLine(RecursiveBaseConversion(8543, 36));
+            int n, b;
+            bool loop = true;
+            while (loop) {
+                if (args.Length < 2) {
+                    Console.Write("Enter number to convert: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Enter base to convert {n} to: ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                } else {
+                    n = Convert.ToInt32(args[0]);
+                    b = Convert.ToInt32(args[1]);
+                }
+                try {
+                    Console.WriteLine($"{n} in decimal is {RecursiveBaseConversion(n, b)} in base{b}.");
+                } catch (NotSupportedException) {
+                    Console.WriteLine($"Base{b} is invalid, try a number from 1-36 inclusively.");
+                }
+                string cont;
+                do {
+                    Console.Write($"Continue? [y/n] ");
+                    cont = Console.ReadLine();
+                } while (!(cont.ToLower().Contains("y") || cont.ToLower().Contains("n")));
+                if (cont.ToLower().Contains("n"))
+                    break;
+            }
         }
 
         //Complete the below method according to the instructions found on the FYE website for assignment 4.
@@ -25,11 +49,12 @@ namespace Assignment4 {
             * @return             The final return will be a complete number in the other base number system (saved as a string to hold anything from 0 - 9, and A - Z).
          */
         public static String RecursiveBaseConversion(int numberToConvert, int numberSystemToConvertTo) {
+            if (numberSystemToConvertTo < 1 || numberSystemToConvertTo > 36)
+                throw new NotSupportedException("Base to convert must be 1-36 inclusively");
             int mod = (numberToConvert % numberSystemToConvertTo);
-            if (numberToConvert <= numberSystemToConvertTo) {
+            if (numberToConvert < numberSystemToConvertTo)
                 return mod <= 9 ? Convert.ToString(mod) : Char.ToString((char)(mod + 55));
-            }
-            return RecursiveBaseConversion((numberToConvert / numberSystemToConvertTo), numberSystemToConvertTo) + (mod <= 9 ? Convert.ToString(mod) : Char.ToString((char)(mod + 55)));
+            return RecursiveBaseConversion(numberToConvert / numberSystemToConvertTo, numberSystemToConvertTo) + (mod <= 9 ? Convert.ToString(mod) : Char.ToString((char)(mod + 55)));
         }
     }
 }
