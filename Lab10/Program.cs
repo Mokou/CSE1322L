9a9e464efcdb2694bf8aad27d70eed5e586588be
Pierce McGoran <pmcgoran@students.kennesaw.edu>
2020-03-22 17:46:54 -0400

    Initial commit of Lab10

---
+++ Lab10/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class MyStack<T> {
+        private LinkedList<T> _list;
+
+        public Stack(){
+            _list = new LinkedList<T>();
+        }
+
+        public override string ToString() {
+            foreach (T i in _list) {
+                ret += $"{i} ";
+            }
+            return ret;
+        }
+
+        public void Push(T item) {
+            _list.Add(item);
+        }
+
+        public T Pop() {
+            T ret = _list.Last.Value;
+            _list.RemoveLast();
+            return ret;
+        }
+
+        public T Peek() {
+            return _list.Last.Value;
+        }
+    }
+
+    class MyQueue<T> {
+        private LinkedList<T> _list;
+
+        public Queue() {
+            _list = new LinkedList<T>();
+        }
+        
+        public override string ToString() {
+            foreach (T i in _list) {
+                ret += $"{i} ";
+            }
+            return ret;
+        }
+        public void Enqueue(T item) {
+            _list.Add(item);
+        }
+
+        public T Dequeue() {
+            T ret = _list.First.Value;
+            _list.RemoveFirst();
+            return ret;
+        }
+
+        public T Peek() {
+            return _list.First.Value;
+        }
+    }
+}
